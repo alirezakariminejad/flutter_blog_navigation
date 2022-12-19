6275dc027bb2d47b58ef2d49ee837c38672d0244
Alireza <alireza.kariminejad@yahoo.com>
2022-12-19 10:26:37 +0330

    Add Image And Buttons To Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'vazir'),
       home: HomeScreen(),
     );
   }
@@ -21,18 +23,50 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueAccent,
       body: SafeArea(
-        child: TextButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return SecondScreen();
-                  },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Text(
+            //   'خوش آمدید',
+            //   style: TextStyle(fontSize: 24.0, color: Colors.white),
+            // ),
+            // SizedBox(height: 30.0),
+            Image(image: AssetImage('images/login.png')),
+            SizedBox(height: 30.0),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Colors.white, width: 1.0),
+                foregroundColor: Colors.white,
+                minimumSize: Size(150, 45),
+                // elevation: 1.0,
+              ),
+              onPressed: () {},
+              child: Text(
+                'ورود به حساب',
+                style: TextStyle(
+                  fontSize: 16.0,
                 ),
-              );
-            },
-            child: Text('Second Screen')),
+              ),
+            ),
+            SizedBox(height: 5.0),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.blueAccent,
+                backgroundColor: Colors.white,
+                minimumSize: Size(150, 45),
+              ),
+              onPressed: () {},
+              child: Text(
+                'ثبت نام',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
