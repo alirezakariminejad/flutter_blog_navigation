69d3cc798da6c40ce9822d41f5e90dde32682099
Alireza <alireza.kariminejad@yahoo.com>
2022-12-21 00:02:01 +0330

    Add AppBar()

--- lib/blog_page.dart
+++ lib/blog_page.dart
@@ -6,7 +6,26 @@ class BlogPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'vazir'),
       home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.amber,
+            ),
+          ),
+          backgroundColor: Colors.blueAccent,
+          // iconTheme: IconThemeData(color: Colors.white),
+          title: Text(
+            'اخبار',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
         body: SafeArea(
           child: Text('Now BlogPage'),
         ),
